dfa5f69350722a7318db1bcd9d4f78ee86af99fa
manasesortez <manasesortez1234@hotmail.com>
2020-11-27 20:21:27 -0600

    wazunga-page

--- src/assets/servicesC.js
+++ src/assets/servicesC.js
@@ -7,38 +7,38 @@ class SolutionC extends PureComponent {
         <div className="div-block-14">
           <img
             className="svg-3"
-            style = {{"width": "120px", height: "150px" ,marginRight:"90px", marginLeft: "90px"}}
+            style = {{"width": "120px", height: "150px" ,marginRight:"5%", marginLeft: "5%"}}
             src={"https://i.ibb.co/Wy4hWf7/Web.png"}
             alt="Web Developer"
           />
           <img
             className="svg-3"
-            style = {{"width": "120px", height: "150px" ,marginRight:"90px", marginLeft: "0px"}}
+            style = {{"width": "120px", height: "150px" ,marginRight:"5%"}}
             src={"https://i.ibb.co/XDxVFCS/Mobile.png"}
             alt="Mobile Developer"
           />
           <img
             className="svg-3"
-            style = {{"width": "120px", height: "150px" ,marginRight:"90px", marginLeft: "0px"}}
+            style = {{"width": "120px", height: "150px" ,marginRight:"5%"}}
             src={"https://i.ibb.co/2WK7w7x/DataBase.png"}
             alt="Data Base"
           />
           <img
           
             className="svg-3"
-            style = {{"width": "120px", height: "150px" ,marginRight:"90px", marginLeft: "0px"}}
+            style = {{"width": "120px", height: "150px" ,marginRight:"5%"}}
             src={"https://i.ibb.co/vwHK8tq/Data-Analytics.png"}
             alt="Data Analytics"
           />
            <img
             className="svg-3"
-            style = {{"width": "120px", height: "150px" ,marginRight:"90px", marginLeft: "0px"}}
+            style = {{"width": "120px", height: "150px" ,marginRight:"5%"}}
             src={"https://i.ibb.co/8bysntg/Vision.png"}
             alt="Computer Vision"/>
             
             <img
             className="svg-3"
-            style = {{"width": "120px", height: "150px" ,marginRight:"90px", marginLeft: "0px"}}
+            style = {{"width": "120px", height: "150px" ,marginRight:"5%"}}
             src={"https://i.ibb.co/VBZcgk2/Software-E.png"}
             alt="Software Daveloper"
           />
--- src/assets/servicesR.js
+++ src/assets/servicesR.js
@@ -20,12 +20,12 @@ class SolutionR extends PureComponent {
     return (
         <div className="div-responsive" style={{ "marginBottom": "10px" }}>
               <Slide {...fadeProperties} >
-                <img alt="service1" src={"https://i.ibb.co/17ctSvW/computadora.png"} style={{ "width": "190px", height: "190px", "marginTop": "20px" }} />
-                <img alt="service2" src={"https://i.ibb.co/DfkW3Qy/ui.png"} style={{ "width": "190px", height: "190px", "marginTop": "20px" }} />
-                <img alt="service3" src={"https://i.ibb.co/QrmZtwJ/nub.png"} style={{ "width": "190px", height: "190px", "marginTop": "20px" }} />
-                <img alt="service4" src={"https://i.ibb.co/TB62cw2/analisis.png"} style={{ "width": "190px", height: "190px", "marginTop": "20px" }} />
-                <img alt="service5" src={"https://i.ibb.co/b53fgx1/vision.png"} style={{ "width": "190px", height: "190px", "marginTop": "20px" }} />
-                <img alt="service6" src={"https://i.ibb.co/V3MW1tp/ordenador.png"} style={{ "width": "190px", height: "190px", "marginTop": "20px" }} />
+                <img alt="service1" src={"https://i.ibb.co/17ctSvW/computadora.png"} style={{ width: "190px", height: "190px", marginTop: "20px" }} />
+                <img alt="service2" src={"https://i.ibb.co/DfkW3Qy/ui.png"} style={{ width: "190px", height: "190px", marginTop: "20px" }} />
+                <img alt="service3" src={"https://i.ibb.co/QrmZtwJ/nub.png"} style={{ width: "190px", height: "190px", marginTop: "20px" }} />
+                <img alt="service4" src={"https://i.ibb.co/TB62cw2/analisis.png"} style={{ width: "190px", height: "190px", marginTop: "20px" }} />
+                <img alt="service5" src={"https://i.ibb.co/b53fgx1/vision.png"} style={{ width: "190px", height: "190px", marginTop: "20px" }} />
+                <img alt="service6" src={"https://i.ibb.co/V3MW1tp/ordenador.png"} style={{ width: "190px", height: "190px", marginTop: "20px" }} />
               </Slide>
         </div>
     )
